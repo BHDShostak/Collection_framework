01885382e278ca136aed0b9767ea3a6ce73d6ed2
BHDShostak <BHDShostak@github.com>
2022-09-13 00:18:49 +0200

    git commit 'Six commit - Update reference to comments'

--- test_app/test_app.py
+++ test_app/test_app.py
@@ -3,31 +3,31 @@ from app.app import count_unique_symbols
 import pytest
 
 
-@functools.lru_cache
+
 def test_cache():
     count_unique_symbols.cache_clear()
     print(count_unique_symbols.cache_info())
 
-
+@functools.lru_cache
 @pytest.mark.parametrize(
     "text",
     [
-        (121321),
-        (12.1321),
-        ((1, 2)),
-        (None),
-        (True),
-        (False),
+        121321,
+        12.1321,
+        (1, 2),
+        None,
+        True,
+        False,
         b'Python Tutorial'
     ]
 )
-@functools.lru_cache
+
 def test_invalid_input(text):
     with pytest.raises(AssertionError):
         count_unique_symbols(text)
     print(count_unique_symbols.cache_info())
 
-
+@functools.lru_cache
 @pytest.mark.parametrize(
     "text, result",
     [
@@ -37,4 +37,3 @@ def test_invalid_input(text):
 )
 def test_valid_input(text, result):
     assert count_unique_symbols(text) == result
-    print(count_unique_symbols.cache_info())
